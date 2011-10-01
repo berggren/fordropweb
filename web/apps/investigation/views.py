e0670e63252c67659e67220ee157322da37e4d74
Johan Berggren <jbn@nordu.net>
2011-10-01 21:46:44 +0200

    Starting to document the view functions

--- web/apps/investigation/views.py
+++ web/apps/investigation/views.py
@@ -1,28 +1,19 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from models import *
-from apps.search.forms import *
-from apps.report.models import *
-from apps.pages.models import *
-from apps.report.utils import *
 from django.contrib.auth.decorators import login_required
-from utils import *
-from forms import *
+from django.core.mail import send_mail
+from web.apps.investigation.models import Investigation, Reference
+from web.apps.pages.models import Page
 from web.apps.report.models import UserFile
 from web.apps.report.forms import UploadFileForm
-
 from web.graphutils import FordropGraphClient
-from django.core.mail import send_mail
-
-
-# Reversion
-#from reversion.models import Version
-#from reversion import revision
-#from web.apps.report.models import UserFile
 from web.utils import investigation_activity_stream
 
 def get_people(investigation):
+    """
+    List of active people in a investigation, based on references
+    """
     people = [investigation.creator]
     for ref in investigation.reference.all():
         ref_files = UserFile.objects.filter(reference=ref)
@@ -35,108 +26,117 @@ def get_people(investigation):
 
 @login_required
 def create(request):
+    """
+    Create a new investigation
+    """
     if request.method == 'POST':
-        references = []
-        for key, value in request.POST.iteritems():
-            if "reference_" in key and value == "on":
-                references.append(key.split("_")[1])
         title = request.POST['title']
-        description, created = Page.objects.get_or_create(title=title   , creator=request.user, content=None)
-        investigation, created = Investigation.objects.get_or_create(title=title, creator=request.user, description=description)
+        description, created = Page.objects.get_or_create(title=title, creator=request.user, content=None)
+        investigation, created = Investigation.objects.get_or_create(title=title,
+                                                                     creator=request.user,
+                                                                     description=description)
         investigation.pages.add(description)
-        for ref in references:
-            reference = Reference.objects.get(name=ref)
-            investigation.reference.add(reference)
-            investigation.save()
+        investigation.investigator.add(request.user)
+        investigation.save()
         url = "/investigation/%i" % investigation.id
         gc = FordropGraphClient()
         gc.add_node(request, investigation, "investigation")
         gc.add_relationship(investigation.creator.get_profile().graphid, investigation.graphid, "created")
-        send_mail('New investigation: %s' % investigation.title, 'A new investigation were created by %s' % investigation.creator.get_full_name(), 'fordrop@django',['jbn@django'], fail_silently=True)
+        send_mail('New investigation: %s' % investigation.title, 'A new investigation were created by %s' %
+                                                                 investigation.creator.get_full_name(),
+                                                                 'fordrop@django',
+                                                                 ['jbn@django'],
+                                                                 fail_silently=True)
         return HttpResponseRedirect(url)
-    investigations = Investigation.objects.all()
-    newinvestigationform = NewInvestigationForm()
-    return render_to_response('apps/investigation/index.html', {'investigations': investigations, 'newinvestigationform': newinvestigationform}, RequestContext(request))
-
-@login_required
-#@revision.create_on_success
-def edit(request, id):
-    investigation = Investigation.objects.get(pk=id)
-    if request.method == 'POST':
-        description = investigation.description
-        description.content = request.POST['description']
-        description.save()
-        #revision.user = request.user
-    return HttpResponseRedirect(request.META["HTTP_REFERER"])
 
 @login_required
 def overview(request, id):
+    """
+    Dashboard of a investigation
+    """
     investigation = Investigation.objects.get(pk=id)
-    uploadform = UploadFileForm()
-    #startdate = UserFile.objects.all().order_by("timecreated")[:1][0].timecreated.strftime('%Y %m %d %H:%M:%S')
-    stream = investigation_activity_stream(investigation.id)
     people = get_people(investigation)
+    upload_form = UploadFileForm()
+    stream = investigation_activity_stream(investigation.id)
     return render_to_response('apps/investigation/overview.html',
                                                                 {
                                                                     'investigation': investigation,
-                                                                    'uploadform': uploadform,
+                                                                    'uploadform': upload_form,
                                                                     'stream': stream,
                                                                     'people': people
                                                                 }, RequestContext(request))
 
 @login_required
 def wiki(request, id):
+    """
+    Simple wiki like system
+    """
     investigation = Investigation.objects.get(pk=id)
     people = get_people(investigation)
-    return render_to_response('apps/investigation/wiki.html', {'investigation': investigation, 'people': people}, RequestContext(request))
+    return render_to_response('apps/investigation/wiki.html',
+                                                            {
+                                                                'investigation': investigation,
+                                                                'people': people
+                                                            }, RequestContext(request))
 
 @login_required
 def timeline(request, id):
+    """
+    Timeline of investigation
+    """
     investigation = Investigation.objects.get(pk=id)
+    people = get_people(investigation)
     files = []
     for ref in investigation.reference.all():
         f = UserFile.objects.filter(reference=ref)
         for file in f:
             files.append(file)
-    people = get_people(investigation)
-    startdate = investigation.timecreated.strftime('%Y %m %d %H:%M:%S')
-    return render_to_response('apps/investigation/timeline.html', {'investigation': investigation, 'startdate': startdate, 'files': files, 'people': people}, RequestContext(request))
+    start_date = investigation.timecreated.strftime('%Y %m %d %H:%M:%S')
+    return render_to_response('apps/investigation/timeline.html',
+                                                                {
+                                                                    'investigation': investigation,
+                                                                    'startdate': start_date,
+                                                                    'files': files,
+                                                                    'people': people
+                                                                }, RequestContext(request))
 
 @login_required
 def library(request, id):
+    """
+    Objects in a investigation
+    """
     investigation = Investigation.objects.get(pk=id)
+    people = get_people(investigation)
     files = []
     for ref in investigation.reference.all():
         ref_files = UserFile.objects.filter(reference=ref)
         for file in ref_files:
             if file not in files:
                 files.append(file)
-    people = get_people(investigation)
-    return render_to_response('apps/investigation/library.html', {'investigation': investigation, 'files': files, 'people': people}, RequestContext(request))
+    return render_to_response('apps/investigation/library.html',
+                                                                {
+                                                                    'investigation': investigation,
+                                                                    'files': files,
+                                                                    'people': people
+                                                                }, RequestContext(request))
 
 @login_required
 def graph(request, id):
+    """
+    Visualization, objects in the graph with the investigation node as reference_node in neo4j
+    Implemented with arbor.js, see the javascript in the template
+    """
     investigation = Investigation.objects.get(pk=id)
-    return render_to_response('apps/investigation/graph.html', {'investigation': investigation}, RequestContext(request))
-
-@login_required
-#@revision.create_on_success
-def page(request, investigation_id, page_id=None):
-    page = None
-    investigation = Investigation.objects.get(pk=investigation_id)
-    if page_id:
-        page = Page.objects.get(pk=page_id)
-    if request.method == 'POST':
-        title = request.POST['title']
-        content = request.POST['content']
-        page, created = Page.objects.get_or_create(title=title, creator=request.user, content=content)
-        investigation.pages.add(page)
-        url = "/investigation/%i/page/%i" % (investigation.id, page.id)
-        return HttpResponseRedirect(url)
-    return render_to_response('apps/investigation/page.html', {'investigation': investigation, 'page': page}, RequestContext(request))
+    return render_to_response('apps/investigation/graph.html',
+                                                            {
+                                                                'investigation': investigation
+                                                            }, RequestContext(request))
 
 @login_required
 def add_reference(request, id):
+    """
+    Add reference to investigation
+    """
     investigation = Investigation.objects.get(id=id)
     if request.method == "POST":
         reference, created = Reference.objects.get_or_create(name=request.POST['reference'])
